065a67c12668bcdc14424d251f21e083aa33d0a7
Yujiro Juanma <yujirodogma22@gmail.com>
2025-11-07 23:22:25 -0600

    Modified lock script for sway

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 PICTURE=/tmp/i3lock.png
-SCREENSHOT="scrot $PICTURE"
+SCREENSHOT="grim $PICTURE"
 
 BLUR="10x5" # Set blur intensity
 
 $SCREENSHOT
 magick $PICTURE -blur $BLUR $PICTURE
-i3lock -i $PICTURE
+swaylock -i $PICTURE
 rm $PICTURE # Removes the temporary image
 
